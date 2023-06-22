aa73ee59943cb79be4fa2476ec02bd5c43688467
Felipe-SSS <6felipe.sss@gmail.com>
2023-06-22 17:14:30 -0300

    First commit

---
+++ SpookStream.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+struct info{
+	char nome[50];
+	int ano_lancamento;
+	char genero1[15];
+	char genero2[15];
+};
+
+int main(){
+	
+	char arq_lista[50];
+	
+}
