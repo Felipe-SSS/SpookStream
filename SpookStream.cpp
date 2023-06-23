e91ba5d1c9e580d3542b9b6456396f734d5e1a09
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2023-06-23 02:38:42 -0300

    modificando entrada de arquivos

--- SpookStream.cpp
+++ SpookStream.cpp
@@ -5,42 +5,49 @@
 using namespace std;
 
 // Struct com as informacoes dos filmes
-struct Filme{ 
-	string nome; // Nome do filme
+struct Filme
+{
+	string nome;		// Nome do filme
 	int ano_lancamento; // Ano de lancamento do filme
-	string genero1; // Genero principal do filme
-	string genero2; // Genero secundario do filme
+	string genero1;		// Genero principal do filme
+	string genero2;		// Genero secundario do filme
 };
 
-int main(){
-	
-	setlocale(LC_ALL, "portuguese");
-	
-	ifstream lista;
+int main()
+{
+
+	// definir localidade para corrigir acentos e caracteres especiais
+	// setlocale(LC_ALL, "portuguese");
+
+	// carregando filmes a partir do arquivo
+	ifstream r_lista; // variavel de leitura
 	string arq_lista = "ListaFilmes.txt";
 	Filme filme[100];
-	int quant = 0;
+	int quant = 0; // quantidade de filmes cadastrados
 
-	lista.open(arq_lista.c_str(), ifstream::in);
-	
-	while(!lista.eof()){
+	r_lista.open(arq_lista.c_str(), ifstream::in);
 
-		getline(lista, filme[quant].nome, '\n');
-		lista >> filme[quant].ano_lancamento;
-		lista.ignore();
-		getline(lista, filme[quant].genero1, '\n');
-		getline(lista, filme[quant].genero2, '\n');
-		quant++; // Vê quantidade total de filmes
+	while (!r_lista.eof())
+	{
 
+		getline(r_lista, filme[quant].nome, '\n');
+		r_lista >> filme[quant].ano_lancamento;
+		r_lista.ignore();
+		getline(r_lista, filme[quant].genero1, '\n');
+		getline(r_lista, filme[quant].genero2, '\n');
+		quant++;
 	}
 
-	for(int i=0; i<quant; i++){
 
-		cout << "Nome do " << i+1 << "º filme: " << filme[i].nome << endl;
-		cout << "Ano de lancamento do " << i+1 << "º filme: " << filme[i].ano_lancamento << endl; 
-		cout << "Genero principal do " << i+1 << "º filme: " << filme[i].genero1 << endl; 
-		cout << "Genero secundario do " << i+1 << "º filme: " << filme[i].genero2 << endl << endl; 
-		 
+	// cout << filme[0].nome << endl;
+	for (int i = 0; i < quant; i++)
+	{
+
+		cout << "Nome do " << i + 1 << "º filme: " << filme[i].nome << endl;
+		cout << "Ano de lancamento do " << i + 1 << "º filme: " << filme[i].ano_lancamento << endl;
+		cout << "Genero principal do " << i + 1 << "º filme: " << filme[i].genero1 << endl;
+		cout << "Genero secundario do " << i + 1 << "º filme: " << filme[i].genero2 << endl
+			 << endl;
 	}
 
 	return 0;
